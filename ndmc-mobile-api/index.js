f1f16fe4e5a2b4e61c853ac0dfbbd2c0f2062c6d
Teklehaimanot Belete <teklehaimanotbelete@Teklehaimanots-MacBook-Pro.local>
2023-12-12 17:36:49 +0300

    mongodb connection is created

--- ndmc-mobile-api/index.js
+++ ndmc-mobile-api/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
 require("dotenv").config();
 
+app.use(cors());
 app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("we are on home page");
+  res.send("We are on the home page");
+});
+
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
