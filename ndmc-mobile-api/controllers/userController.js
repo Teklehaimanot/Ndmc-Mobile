22526d8813962f02267dd12ee45c60d2ce46569b
Teklehaimanot Belete <teklehaimanotbelete@Teklehaimanots-MacBook-Pro.local>
2023-12-13 12:10:21 +0300

    email validation for registration

--- ndmc-mobile-api/controllers/userController.js
+++ ndmc-mobile-api/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const isValidEmail = require("../utility/validateEmail");
 require("dotenv/config");
 
 const createUser = async (req, res) => {
@@ -14,15 +15,17 @@ const createUser = async (req, res) => {
 
   try {
     if (!name || !email || !role || !password) {
-      res.status(400).json({ error: "all fileds are required" });
+      return res.status(400).json({ error: "all fileds are required" });
     }
 
     if (password && password.length < 7) {
-      res
+      return res
         .status(400)
         .json({ error: "Min password length should be atleast 7" });
     }
-
+    if (!isValidEmail(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
     const saltRound = 10;
     const passwordHash = await bcrypt.hash(password, saltRound);
     const user = await User.create({
@@ -33,7 +36,6 @@ const createUser = async (req, res) => {
     });
     const userData = _.pick(user, ["_id", "email"]);
     const accessToken = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET);
-    console.log(accessToken);
     res.header("x-auth", `Bearer ${accessToken}`).json({
       success: true,
       token: `Bearer ${accessToken}`,
@@ -45,6 +47,41 @@ const createUser = async (req, res) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  let { email, password } = _.pick(req.body, ["email", "password"]);
+  try {
+    if (!password || !email) {
+      return res
+        .status(400)
+        .json({ error: "Password and username are required!" });
+    }
+    const user = await User.find({ email });
+    if (!user) {
+      return res.status(400).json({ error: "User not found!" });
+    }
+    console.log(user, password);
+    const isValid = await bcrypt.compare(password, user[0].password);
+
+    if (!isValid) {
+      return res
+        .status(400)
+        .json({ error: "Invalid detail please check password and username!" });
+    }
+
+    const userData = _.pick(user[0], ["_id", "username", "name"]);
+    const accessToken = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET);
+    res.header("x-auth", `Bearer ${accessToken}`).json({
+      success: true,
+      token: `Bearer ${accessToken}`,
+      role: user[0].role,
+    });
+  } catch (err) {
+    console.log({ error: err });
+    res.status(400).json({ Error: "Some error" });
+  }
+};
+
 module.exports = {
   createUser,
+  loginUser,
 };
