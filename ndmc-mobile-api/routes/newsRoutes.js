351c10202868d887606ac924a3b5bafeabee1f7f
Teklehaimanot Belete <teklehaimanotbelete@Teklehaimanots-MacBook-Pro.local>
2023-12-18 15:11:35 +0300

    adding middle wares on the creating news api

--- ndmc-mobile-api/routes/newsRoutes.js
+++ ndmc-mobile-api/routes/newsRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const newsController = require("../controllers/newsController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const roleMiddleware = require("../middlewares/roleMiddleware");
 
 const newsRouter = express.Router();
 
-newsRouter.post("/", newsController.createNews);
+newsRouter.post(
+  "/",
+  authMiddleware,
+  roleMiddleware("admin"),
+  newsController.createNews
+);
 newsRouter.post(
   "/createComment/:newsId",
   authMiddleware,
