9fd54e19ab424971205778597cb16fa6e8729802
Teklehaimanot Belete <teklehaimanotbelete@Teklehaimanots-MacBook-Pro.local>
2023-12-27 15:02:30 +0300

    handling logout button

--- ndmc-web-app/src/pages/Home.js
+++ ndmc-web-app/src/pages/Home.js
@@ -1,8 +1,27 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../state/auth/authSlice";
 
 const Home = () => {
-  const auth = useSelector((state) => state.auth);
-  console.log(auth);
-  return <div>{auth.user?.email} teki</div>;
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch(logout());
+
+  const handleLogout = async () => {
+    dispatch(logout());
+    localStorage.removeItem("token");
+  };
+  return (
+    <div className="flex flex-row justify-between bg-primary text-secondary py-4 items-center">
+      <div className="mx-5">NDMC - {user.role.toUpperCase()}</div>
+      <div className="flex flex-row w-1/2 justify-end items-center space-x-8 mx-5">
+        <div className="text-xl">{user.name}</div>
+        <button
+          className="border rounded p-1 hover:bg-blue"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 };
 export default Home;
