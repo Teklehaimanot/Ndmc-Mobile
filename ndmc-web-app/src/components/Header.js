30fdea2dbb5c2294925a48f8f6822e91d4c7c429
teklehaimanot <teklehaimanotbelete@gmail.com>
2023-12-29 15:16:34 +0300

    handling logout api in the frontend side

--- ndmc-web-app/src/components/Header.js
+++ ndmc-web-app/src/components/Header.js
@@ -1,16 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
 import { IoIosArrowForward } from "react-icons/io";
 import { logout } from "../state/auth/authSlice";
+import axios from "axios";
 
 const Header = () => {
   const { user } = useSelector((state) => state.auth);
   const { catagory } = useSelector((state) => state.catagory);
-  const dispatch = useDispatch(logout());
+  const basicUrl = process.env.REACT_APP_BACKEND_URL;
+  const dispatch = useDispatch();
 
   const handleLogout = async () => {
-    dispatch(logout());
-    localStorage.removeItem("token");
+    const response = await axios.get(`${basicUrl}/user/logout/${user.id}`);
+    const { data } = response;
+    if (data) {
+      dispatch(logout());
+      localStorage.removeItem("token");
+    }
   };
+  console.log("u", user);
   return (
     <div className="flex flex-row justify-between bg-primary text-secondary py-4 items-center border-b border-black shadow-md">
       <div className="mx-5 flex flex-row justify-between items-center space-x-1">
